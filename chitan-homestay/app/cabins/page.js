83b9d27c0127be0b4d985c033df844c4dc0fddcd
dochtan1608 <89788071+dochtan1608@users.noreply.github.com>
2024-10-07 06:16:48 +0700

    counter
    json placeholder / loading page / counter side

--- chitan-homestay/app/cabins/page.js
+++ chitan-homestay/app/cabins/page.js
@@ -1,7 +1,18 @@
-export default function Page() {
+import Counters from "../components/Counters";
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
   return (
     <div>
       <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counters users={data} />
     </div>
   );
 }
--- chitan-homestay/app/page.js
+++ chitan-homestay/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <h1>ChiTan Homestay. Welcome to paradise</h1>
+      <h1>CHITAN Homestay. Welcome to paradise</h1>
       <Link href="/cabins">Explore luxury cabins</Link>
     </div>
   );
